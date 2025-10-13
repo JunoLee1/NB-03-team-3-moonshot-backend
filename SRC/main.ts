ad74684cd0fb0fee4fd7bdbf803c491508b2f714
JunoLee1 <99115649+JunoLee1@users.noreply.github.com>
2025-10-13 10:03:25 +0900

    main

--- SRC/main.ts
+++ SRC/main.ts
@@ -11,5 +11,5 @@ dotenv.config();
 // app.use("/users")
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+console.log(`Server is running on http://localhost:${PORT}`);
 });
