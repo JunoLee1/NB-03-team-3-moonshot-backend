664cab1080edd2db3ea882025afd86f430461290
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-16 18:47:14 +0900

    feat: Add api endpoint for project update

--- SRC/project/project-router.ts
+++ SRC/project/project-router.ts
@@ -1,24 +1,21 @@
-import express  from "express";
+import express from "express";
 import { ProjectController } from "./project-controller.js";
 // 추후 인증 및 필요한 미들웨어 추가예정
 
 const projectRouter = (
-    projectController: ProjectController,
-    // 미들웨어 추가
+  projectController: ProjectController
+  // 미들웨어 추가
 ) => {
-    const router = express.Router();
+  const router = express.Router();
 
-    router
-        .route('/')
-        .post(
-            // 미들웨어 추가 가능
-            projectController.createProject,
-        );
+  router.route("/").post(
+    // 미들웨어 추가 가능
+    projectController.createProject
+  );
 
-    router
-        .route('/:projectId')
-        .get(
-            projectController.getProjectById,
-        )
-        return router;
+  router
+    .route("/:projectId")
+    .get(projectController.getProjectById)
+    .patch(projectController.updateProject);
+  return router;
 };
