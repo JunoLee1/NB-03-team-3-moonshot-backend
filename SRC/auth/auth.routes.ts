0dcd1943f8a05247feb1d4e420f3010d7347207f
JunoLee1 <99115649+JunoLee1@users.noreply.github.com>
2025-10-16 13:21:34 +0900

    authrouter

--- SRC/auth/auth.routes.ts
+++ SRC/auth/auth.routes.ts
@@ -11,5 +11,8 @@ router.post("/login",async (req: Request, res: Response, next: NextFunction)=>{
     authController.loginController(req, res, next)
 });
 //회원가입
+router.post("/register",async(req: Request, res: Response, next: NextFunction)=>{
+    authController.siginupController(req, res, next)
+})
 
 export default router;
