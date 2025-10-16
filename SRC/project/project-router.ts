e38dd788a3e4febf89bc830cd7659bf70a056e2d
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-16 09:41:46 +0900

    fead: Add api endpoti for project creation

---
+++ SRC/project/project-router.ts
@@ -0,0 +1,19 @@
+import express  from "express";
+import { ProjectController } from "./project-controller.js";
+// 추후 인증 및 필요한 미들웨어 추가예정
+
+const projectRouter = (
+    projectController: ProjectController,
+    // 미들웨어 추가
+) => {
+    const router = express.Router();
+
+    router
+        .route('/')
+        .post(
+            // 미들웨어 추가 가능
+            projectController.createProject,
+        )
+
+        return router;
+};
