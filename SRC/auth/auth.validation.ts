fed294595f800db2b6a9ddc98486c16ca2d0d08d
Calvin Lee <jinwoo0451@naver.com>
2025-10-16 21:33:47 +0900

    미들웨어 넘길때, user 관련 피드백 추가

--- SRC/auth/auth.validation.ts
+++ SRC/auth/auth.validation.ts
@@ -2,10 +2,10 @@ import { Request,Response,NextFunction } from "express";
 import {z} from "zod";
 // auth schema
 const authRegisterSchema = z.object({
-    id: z.number().positive().optional(),
+    // 만약 id를 클라이언트에서 받는거라면 id는 필요없으니 삭제를 하자.
+    // id: z.number().positive().optional(),
     email: z.string().email(),
     password:z.string().min(6,"6글자 이상이어야합니다").optional(),
-    
 })
 const authLoginShema = z.object({
     email: z.string().email(),
