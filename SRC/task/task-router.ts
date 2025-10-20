11ad723c428180a016a34c2f022438f95ff2d7ad
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-20 15:26:59 +0900

    feat: Add api endpoint for task creation

---
+++ SRC/task/task-router.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { TaskController } from "./task-controller.js";
+// 추후 인증 미들웨어 import
+
+const taskRouter = (taskController: TaskController) => {
+  const router = express.Router({ mergeParams: true });
+
+  router.route("/").post(
+    // 추후 인증 미들웨어 추가 필요
+    taskController.createTask
+  );
+  return router;
+};
+
+export default taskRouter;
