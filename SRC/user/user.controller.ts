7622f545bd57ba0a4edf9bc8587b2af0898af17a
JunoLee1 <99115649+JunoLee1@users.noreply.github.com>
2025-10-20 20:13:16 +0900

    [fix]user

--- SRC/user/user.controller.ts
+++ SRC/user/user.controller.ts
@@ -5,7 +5,7 @@ import prisma from "../lib/prisma.js";
 import { number } from "zod/v4";
 
 export interface IUserDTO {
-  id?: number ;
+  id: number ;
   nickname: string;
   email: string;
   createdAt?: Date;
@@ -16,12 +16,19 @@ const userService = new UserService();
 export default class UserController {
   async userInfoController(req: Request, res: Response, next: NextFunction) {
     try {
-      console.log("userInfoController의 req.params:", req.params);
-      const { id } = (req as any).user; // 인증 미들웨어에서 req.query id넣어주기
-    
-      console.log("id", id);
-      const { nickname, email } = req.body;//validation 미들웨어에서 req.body에 nickname, email 확인후 넣어주기
-      console.log("nickname, email", nickname, email);// ❌ undefined 오류
+      // 인증 미들웨어에서 req.query id넣어주기
+      const { id, nickname, email } = req.body as {
+        id :unknown,
+        nickname: unknown,
+        email:unknown,
+      } ;//validation 미들웨어에서 req.body에 nickname, email 확인후 넣어주기
+
+      if(typeof email !== "string" || !email.includes("@")) 
+        throw new HttpError(400, "해당 유저의 이메일은 문자열이어야하고 이메일 형식 이어야합니다")
+
+      if (typeof nickname !== "string")
+        throw new HttpError(400, "해당 유저의 닉네임은 문자열이어야합니다");
+
       const unique_check = await userService.getUserInfoById({
         id: Number(id),
         email,
@@ -31,21 +38,10 @@ export default class UserController {
       if (!unique_check) {
         throw new HttpError(404, "해당 유저가 존재하지 않습니다");
       };
-      if (typeof nickname !== "string")
-        throw new HttpError(400, "해당 유저의 닉네임은 문자열이어야합니다");
-      if (typeof email !== "string" && email.includes("@"))
-        throw new HttpError(
-          400,
-          "해당 유저의 이메일은 문자열이어야하고 이메일 형삭이 아니어야합니다"
-        );
-      if (typeof id !== "number" && id > 0)
-        throw new HttpError(
-          400,
-          "해당 유저의 인덱스는 0보다 큰정수이 어야합니다"
-        );
-
+     
+      
       const userInfo = await userService.getUserInfoById({
-        id,
+        id:Number(id),
         email,
         nickname,
       });
@@ -60,10 +56,12 @@ export default class UserController {
   async userUpdateController(req: Request, res: Response, next: NextFunction) {
     const { nickname: rawNickname, email: rawEmail } = req.body;
     try {
-      if(!req.user) throw new HttpError(401,"unauthorization")
+      if(!req.user?.id) throw new HttpError(401,"unauthorization")
 
       const userId = req.user.id; // 인증 미들웨어에서 req.query id넣어주기
       const id = Number(userId)
+      const nickname = String(rawNickname);
+      const email = String(rawEmail);
       const unique_check = await userService.getUserInfoById({
         id,
         email: String(rawEmail),
@@ -72,9 +70,7 @@ export default class UserController {
       if (!unique_check) {
         throw new HttpError(404, "해당 유저가 존재하지 않습니다");
       }
-    
-      const nickname = String(rawNickname);
-      const email = String(rawEmail);
+     
 
       const updatedUser = await userService.updatedUser({
         id: Number(id),
@@ -96,9 +92,10 @@ export default class UserController {
     next: NextFunction
   ) {
     try {
-      if(!req.user) throw new HttpError(401,"unauthorization")
-      const userId= req.user.id; // 인증 미들웨어에서 req.query id넣어주기
-      const projects = await userService.findUserProjects({ userId });
+      if(!req.user?.id) throw new HttpError(401,"unauthorization"); // 인증 미들웨어에서 req.query id넣어주기
+      
+      const userId = req.user.id
+      const projects = await userService.findUserProjects({userId});
       return res.json({
         success: true,
         data: projects,
@@ -107,9 +104,11 @@ export default class UserController {
       next(error);
     }
   }
+
   async findUserTasksController(req: Request, res: Response, next: NextFunction) {
-    if(!req.user) throw new HttpError(401,"unauthorization")
-    const { id } = req.user // 인증 미들웨어에서 req.user id넣어주기
+    if(!req.user?.id) throw new HttpError(401,"unauthorization")
+    // 인증 미들웨어에서 req.user id넣어주기
+    const userId = req.user.id
     try {
       const taskIdRaw = req.query.task_id;
       if(typeof taskIdRaw !== "string" && isNaN(Number(taskIdRaw))){
@@ -127,7 +126,7 @@ export default class UserController {
         throw new HttpError(404, "Bad request");
       }
       const taskId = String(string_task);
-      const result = await userService.findUserTasks({ taskId: taskId, userId :Number(id)});
+      const result = await userService.findUserTasks({ taskId: taskId, userId :Number(userId)});
       return res.json({
         success: true,
         data: result,
