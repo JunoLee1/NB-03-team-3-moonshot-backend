2ee3890beeda664032c3b64d084eb27b0efd9aad
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-21 11:20:30 +0900

    feat: Add task service code for inquiry the task list

--- SRC/task/task-service.ts
+++ SRC/task/task-service.ts
@@ -4,8 +4,10 @@ import {
   TaskResponseDto,
   AssigneeDto,
   TaskStatusType,
+  TaskQueryDto,
+  TaskListResponseDto,
 } from "./task-dto.js";
-import { PrismaClient } from "@prisma/client"; // prisma 추후 레포지토리 계층에서만 사용할 수 있도록 리팩토링 하는게 좋을 것 같음. (관심사 분리)
+import { PrismaClient, Task } from "@prisma/client"; // prisma 추후 레포지토리 계층에서만 사용할 수 있도록 리팩토링 하는게 좋을 것 같음. (관심사 분리)
 
 // 추후 별도 에러 파일로 분리하는 것이 좋을 거 같음
 class ForbiddenException extends Error {
@@ -95,6 +97,109 @@ export class TaskService {
         attachments: rawTaskData.attachments,
         createdAt: rawTaskData.createdAt,
         updatedAt: rawTaskData.updatedAt,
+      };
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  /**
+   * @param userId 요청한 사용자의 ID
+   * @param projectId 조회할 프로젝트의 ID
+   * @param query 컨트로럴에서 받은 req.query 객체
+   * @returns { data: TaskResponseDto[], total: number }
+   */
+  getTasks = async (
+    userId: number,
+    projectId: number,
+    query: any
+  ): Promise<TaskListResponseDto> => {
+    try {
+      // 요청한 유저가 이 프로젝트의 멤버인지 확인
+      const member = await this.prisma.member.findFirst({
+        where: {
+          user_id: userId,
+          projectId: projectId,
+        },
+      });
+
+      if (!member) {
+        throw new ForbiddenException(
+          "할 일 목록을 조회할 권한이 없습니다. 프로젝트 멤버가 아닙니다."
+        );
+      }
+
+      // 쿼리 파라미터 파싱 및 타입 변환, 기본값 우선 정의
+      const options: TaskQueryDto = {
+        page: query.page ? parseInt(String(query.page), 10) : 1,
+        limit: query.limit ? parseInt(String(query.limit), 10) : 10,
+        order: query.order ? (query.order as "asc" | "desc") : "desc",
+        order_by: query.order_by
+          ? (query.order_by as "created_at" | "name" | "end_date")
+          : "created_at",
+      };
+
+      // 값이 존재하면 객체에 속성 추가
+      if (query.status) {
+        options.status = query.status as TaskStatusType;
+      }
+      if (query.assignee) {
+        options.assignee = parseInt(String(query.assignee), 10);
+      }
+      if (query.keyword) {
+        options.keyword = String(query.keyword);
+      }
+
+      // 파싱된 숫자 유효성 검사
+      if (
+        Number.isNaN(options.page) ||
+        Number.isNaN(options.limit) ||
+        (options.assignee && Number.isNaN(options.assignee))
+      ) {
+        throw new Error("page와 limit는 숫자여야 합니다."); // 추후 커스텀 에러 적용이 좋을 거 같음
+      }
+
+      const { data: rawTasks, total } = await this.taskRepository.getTasks(
+        projectId,
+        options
+      );
+
+      // 데이터 가공, createTasks 메서드의 변환로직과 중복, 추후 다른 메서드로 분리하면 좋을 거 같음
+      const taskDtos: TaskResponseDto[] = rawTasks.map((task) => {
+        const assigneeUser = task.members.users;
+        const assignee: AssigneeDto | null = assigneeUser
+          ? {
+              id: assigneeUser.id,
+              name: assigneeUser.nickname,
+              email: assigneeUser.email,
+              profileImage: assigneeUser.image,
+            }
+          : null;
+
+        return {
+          id: task.id,
+          projectId: task.project_id,
+          title: task.title,
+          startYear: task.start_year,
+          startMonth: task.start_month,
+          startDay: task.start_day,
+          endYear: task.end_year,
+          endMonth: task.end_month,
+          endDay: task.end_date,
+          status: task.taskStatus as TaskStatusType,
+          assignee: assignee,
+          tags: task.tags,
+          attachments: task.attachments,
+          createdAt: task.createdAt,
+          updatedAt: task.updatedAt,
+        };
+      });
+
+      // 최종 응답 DTO 반환
+      return {
+        data: taskDtos,
+        total: total,
       };
     } catch (error) {
       console.error(error);
