07040b9d03d01b664d42420d31282715f23029d4
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-22 18:16:49 +0900

    feat: Connect task and project router in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,27 @@ import error_handler from "./middleware/errorMiddle.js";
 import userRouter from "./user/user.routes.js";
 import authRouter from "./auth/auth.routes.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-
+import cors from "cors";
+import container from "./container.js";
 
 const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 const app = express();
 
-app.use("/user", userRouter);
+app.use("/user", userRouter); // 연결 순서 변경 필요
 app.use("/auth", authRouter);
-app.use(cookieParser())
-app.use(cors())
+app.use(cookieParser());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const { projectRouterInstance, mainTaskRouterInstance } =
+  container.getRouters();
+
+app.use("/projects", projectRouterInstance);
+app.use("/tasks", mainTaskRouterInstance);
 
 app.use(error_handler);
 
