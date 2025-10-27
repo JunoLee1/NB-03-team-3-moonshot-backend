8955444fa5dba65df422046a5e374039ec2e7dbf
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-10-27 13:48:48 +0900

    feat: Update DI container for linked google calendar

--- src/container.ts
+++ src/container.ts
@@ -12,6 +12,7 @@ import { TaskRepository } from "./task/task-repository.js";
 import { TaskService } from "./task/task-service.js";
 import { TaskController } from "./task/task-controller.js";
 import { nestedTaskRouter, mainTaskRouter } from "./task/task-router.js";
+import { GoogleCalendarService } from "./lib/google-calendar-service.js";
 
 // 미들웨어 임포트
 import { authMiddleWare } from "./middleware/authMiddle.js";
@@ -29,6 +30,7 @@ class Container {
   public readonly projectService: ProjectService;
   public readonly emailService: EmailService;
   public readonly taskService: TaskService;
+  public readonly googleCalendarService: GoogleCalendarService;
 
   // Controller
   public readonly projectController: ProjectController;
@@ -50,7 +52,12 @@ class Container {
       prisma,
       this.emailService
     );
-    this.taskService = new TaskService(this.taskRepository, prisma);
+    this.googleCalendarService = new GoogleCalendarService();
+    this.taskService = new TaskService(
+      this.taskRepository,
+      prisma,
+      this.googleCalendarService
+    );
 
     // Controller 인스턴스 생성
     this.projectController = new ProjectController(this.projectService);
